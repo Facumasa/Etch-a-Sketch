2dccfe486af2b557fb457e14945f87efbe6493f1
Facumasa <facundomasari@gmail.com>
2025-03-21 10:48:19 +0100

    DOesnt work now

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ let numSquaresGlobal = 40;
 const squaresButton = document.createElement("button"); // Create button
 squaresButton.classList.add("button1"); // Add a class for CSS styling (class name is button1)
 
-squaresButton.textContent = "Number of Squares per side"; // Set button text
+squaresButton.textContent = "Number of Squares side"; // Set button text
 
 // document.body.appendChild(squaresButton); // Add the button to the page (this would ad it at the bottom of the body created from the html file, so AFTER the container)
 document.body.prepend(squaresButton); //the prepend method appends to the body but before any other child present in the body
@@ -18,6 +18,7 @@ const container = document.querySelector("#container");
 
 // function that creates all the divs
 function createDivDraw (size) {
+    document.getElementById("container").replaceChildr0en(); //Borra todos los divs hijos de container (todos los cuadraditos que existian)
     for (let j=0; j < size; j++) {
         const rowDiv = document.createElement("div");
         rowDiv.classList.add('rowstyle');
@@ -57,11 +58,11 @@ document.body.appendChild(deleteButton); // Add the button to the page
 
 squaresButton.addEventListener("click", () => {
     // prompt box syntax
-    let numberSquares = prompt("Please enter your name", "Just the number here");
+    let numberSquares = prompt("Please enter the number of squares per side for the new grid", "Just the number here");
     if (numberSquares == null || numberSquares == "" || isNaN(numberSquares)) {
       alert("Error, number of squares assumed 40");
       numberSquares = 40;
     }
     numSquaresGlobal = parseInt(numberSquares); //updates the value to the number given
-    
+    createDivDraw(numSquaresGlobal);
 });
